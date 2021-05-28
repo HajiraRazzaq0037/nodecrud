51f04da7a8cd97f47c701f8d9d623535ff40b118
hajira0037 <hajira@med-examexpert.com>
2021-05-28 22:26:55 +0500

    redis cache in node js

--- controller/tempController.js
+++ controller/tempController.js
@@ -1,25 +1,73 @@
-const request = require("request");
+const axios = require("axios");
+const redis = require("redis");
 const constant = require("../const/config");
+
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+  console.log(err);
+});
 const getTemp = (req, res, next) => {
-  let url = `${constant.baseUrl}weather?q=${req.query.city}&appid=${constant.apiKey}`;
-  request(url, function (err, response, body) {
-    if (err) {
-      res.send(JSON.parse(err));
-    } else {
-      res.send(JSON.parse(body));
-    }
-  });
+  const city = req.query.city;
+  try {
+    client.get(city, async (err, data) => {
+      if (err) throw err;
+      if (data) {
+        // res.status(200).send({
+        //   data: JSON.parse(data),
+        //   message: "data retrieved from the cache",
+        // });
+        console.log("datafrom cache");
+        res.send(data);
+      } else {
+        let url = `${constant.baseUrl}weather?q=${city}&appid=${constant.apiKey}`;
+        axios
+          .get(url)
+          .then((response) => {
+            client.setex(city, 600, JSON.stringify(response.data));
+            res.send(response.data);
+          })
+          .catch((error) => {
+            res.send(error);
+          });
+      }
+    });
+  } catch (err) {
+    res.send(err.message);
+  }
 };
 
 getSevenDayForCast = (req, res, next) => {
-  let url = `${constant.baseUrl}onecall?lat=${req.query.latitude}&lon=${req.query.longitude}&exclude=hourly,minutely&appid=${constant.apiKey}`;
-  request(url, function (err, response, body) {
-    if (err) {
-      res.send(JSON.parse(err));
-    } else {
-      res.send(JSON.parse(body));
-    }
-  });
+  const {
+    query: { longitude, latitude },
+  } = req;
+  try {
+    client.get("temp", async (err, data) => {
+      if (err) throw err;
+      if (data) {
+        // res.status(200).send({
+        //   data: JSON.parse(data),
+        //   message: "data retrieved from the cache",
+        // });
+        console.log("datafrom cache");
+        res.send(data);
+      } else {
+        let url = `${constant.baseUrl}onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${constant.apiKey}`;
+        axios
+          .get(url)
+          .then((response) => {
+            client.setex("temp", 600, JSON.stringify(response.data));
+            res.send(response.data);
+          })
+          .catch((error) => {
+            res.send(error);
+          });
+      }
+    });
+  } catch (err) {
+    res.send(err.message);
+  }
 };
 module.exports = {
   getTemp,
